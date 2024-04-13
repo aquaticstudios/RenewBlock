fef56c02cbbd13fa5522af24ceeae385f7a4e33a
Aquatic <sxntido@gmail.com>
2024-04-13 20:01:29 -0300

    public class Tab implements org.bukkit.command.TabCompleter {

---
+++ src/main/java/team/aquatic/studios/actions/Tab.java
@@ -0,0 +1,24 @@
+package team.aquatic.studios.actions;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tab implements TabCompleter {
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completions = new ArrayList<>();
+
+        if (args.length == 1) {
+            if (sender.hasPermission("renewblock.admin")) {
+                completions.add("help");
+                completions.add("reload");
+            }
+        }
+        return completions;
+    }
+}
